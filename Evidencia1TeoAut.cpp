032a5557c8eedc3b3264db9fc3a44050266d6f0a
AngelF4 <angelfhernandez71@gmail.com>
2023-09-10 15:22:41 -0600

    interacción con usuario

--- Evidencia1TeoAut.cpp
+++ Evidencia1TeoAut.cpp
@@ -5,9 +5,10 @@ using namespace std;
 
 main(){
 	string nombre;
+	string cadena;
 	int matricula = 0;
 	
-	cout<< "Programa de Teoria de Automatas\n";
+	cout<< "Programa de Teoria de Automatas" << endl;
 	cout << "\nPrimero vamos a necesitar tu nombre y matricula para obtener el alfabeto\n";
 	cout<< "Introduce tu nombre completo: ";
 	getline(cin, nombre);
@@ -16,5 +17,10 @@ main(){
 	
 	//Aqui va donde se crearia el alfabeto con las funciones
 	
+	cout << "Listo, ya tenemos el alfabeto, ahora ingresa una cadena de texto" << endl;
+	cout << "para revisar si es valida o no\nCadena: ";
+	cin.sync();
+	getline(cin, cadena);
+	cout << cadena;
 	
 }
