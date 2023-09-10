e6fe48ac9cf19a375415a93fab13605117af8668
AngelF4 <angelfhernandez71@gmail.com>
2023-09-10 15:08:50 -0600

    Obtención de datos basicos

--- Evidencia1TeoAut.cpp
+++ Evidencia1TeoAut.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 main(){
-	cout << "hello world";
+	string nombre;
+	int matricula = 0;
+	
+	cout<< "Programa de Teoria de Automatas\n";
+	cout << "\nPrimero vamos a necesitar tu nombre y matricula para obtener el alfabeto\n";
+	cout<< "Introduce tu nombre completo: ";
+	getline(cin, nombre);
+	cout << "Gracias\nAhora Introduce tu matricula: ";
+	cin >> matricula;
+	
+	//Aqui va donde se crearia el alfabeto con las funciones
+	
+	
 }
